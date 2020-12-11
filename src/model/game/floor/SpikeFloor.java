4930db4afa2b80e2714fe4d329955f0c693ffd7e
Nitrola <goetzalexandre54@gmail.com>
2020-12-11 09:02:55 +0100

    documentation SpikeFloor.java

--- src/model/game/floor/SpikeFloor.java
+++ src/model/game/floor/SpikeFloor.java
@@ -15,6 +15,14 @@ public class SpikeFloor extends ActivateFloor {
     private final BufferedImage  imSpikeDown=ImageIO.read(new File("resources/images/floorspikedown.png"));
     private final BufferedImage  imSpikeUp=ImageIO.read(new File("resources/images/floorspikeup.png"));
     private int delay;
+
+    /**
+     *
+     * @param p position dans le labyrinthe
+     * @param w largeur dans la frame
+     * @param h hauteur dans la frame
+     * @throws IOException erreur chargement image
+     */
     public SpikeFloor(Point p, int w, int h) throws IOException {
         super(p, w, h);
         im = imSpikeUp;
@@ -32,6 +40,12 @@ public class SpikeFloor extends ActivateFloor {
         }},0,this.delay);
     }
 
+    /**
+     * Fait des dégats aux héro si les piques sont sorties
+     * @param hero héro à qui faire des dégats
+     * @param maze labyrinthe dans lequel les piques évoluent
+     * @throws IOException
+     */
     @Override
     public void activate(Hero hero, Maze maze) throws IOException {
         if(im == imSpikeUp){
@@ -58,15 +72,27 @@ public class SpikeFloor extends ActivateFloor {
         }
     }
 
+    /**
+     * desactive le piège
+     * @throws IOException image non chargée
+     */
     @Override
     public void desactivate() throws IOException {
         im = ImageIO.read(new File("resources/images/normalfloor.png"));
     }
 
+    /**
+     *
+     * @return temps entre la sortie et la rentré des piques
+     */
     public int getDelay() {
         return delay;
     }
 
+    /**
+     *
+     * @param delay interval de temps pour sortir et rentrer les pique
+     */
     public void setDelay(int delay) {
         this.delay = delay;
     }
